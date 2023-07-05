6b72c493bb3f5429be193ed816d5ec835d9d9adc
DmitryBelo <Bdsxx@yandex.ru>
2023-07-05 14:43:54 +0300

    Create Практическая работа 1.4.py

---
+++ Практическая работа 1.4.py
@@ -0,0 +1,26 @@
+# Конструкция while
+
+'''while условие | (условие):
+    действие...
+    действие...'''
+    
+'''number = 5
+while number > 0:
+    print('Привет')
+    number = number - 1
+    print(number)'''
+    
+    
+'''for i in range(3, 20, 3):
+        print(i)
+        
+        
+list_numbers = [12, 5, 7, 9, 10,]
+list_bool = [True, False, False, True]
+list_diffrent = [23, 'fff', True]
+
+list_numbers.append(30)
+print(list_numbers)'''
+
+
+
